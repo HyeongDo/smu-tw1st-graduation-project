dea37ab01d02c308918964106409cefb3929e49f
201411115 <qkrrhltlr@naver.com>
2019-08-16 01:32:48 +0900

    정렬 및 가격 String 형식 통일

---
+++ app/src/main/java/com/smu/tw1st/CustomDialog.java
@@ -0,0 +1,46 @@
+package com.smu.tw1st;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomDialog extends ProgressDialog {
+
+    private Context c;
+    private ImageView imgLogo;
+    public CustomDialog(Context context) {
+        super(context);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        setCanceledOnTouchOutside(false);
+
+        c=context;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_dialog);
+        imgLogo = (ImageView) findViewById(R.id.iv_frame_loading);
+        final AnimationDrawable frameAnimation = (AnimationDrawable) imgLogo.getBackground();
+        frameAnimation.start();
+        TextView tv_progress_message = (TextView)findViewById(R.id.tv_progress_message);
+        tv_progress_message.setText("항공권 조회중 ....");
+
+    }
+    @Override
+    public void show() {
+        super.show();
+    }
+    @Override
+    public void dismiss() {
+        super.dismiss();
+    }
+}
